5023c4c593bdbc163af3482e472fc967395cfd3e
BaeDaBin <dabin@albacheck.com>
2021-10-17 16:23:23 +0900

    fix(master): 공통 헤더 추가

---
+++ src/components/Common/SubHeader_backup.vue
@@ -0,0 +1,30 @@
+<template>
+	<header>
+		<div class="left-btn">
+			<button @click="$emit('left')">
+				<slot name="left" />
+			</button>
+		</div>
+		<h2 class="title">
+			{{ title }}
+		</h2>
+		<div class="right-btn">
+			<button @click="$emit('right')">
+				<slot name="right" />
+			</button>
+		</div>
+	</header>
+</template>
+
+<script>
+export default {
+	name: 'SubHeader',
+	props: ['title']
+};
+</script>
+
+<style lang="scss" scoped>
+header {
+	display: flex;
+}
+</style>
